5ce539a2f6ae73c4bec888a1c0cfb3bbc4991097
Andrey Marten <andrey.kunitsyn@devexpress.com>
2026-04-24 12:15:41 +0200

    Apply changes to VB version of the project

--- VB/PrismOnDXDocking.ExampleModule/Views/DocumentView.xaml.vb
+++ VB/PrismOnDXDocking.ExampleModule/Views/DocumentView.xaml.vb
@@ -1,21 +1,17 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.ComponentModel.Composition
-Imports System.Linq
-Imports System.Windows.Controls
-Imports PrismOnDXDocking.Infrastructure
+﻿Imports System.Windows.Controls
 
 Namespace PrismOnDXDocking.ExampleModule.Views
-	<PartCreationPolicy(CreationPolicy.NonShared), Export> _
-	Partial Public Class DocumentView
-		Inherits UserControl
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Public ReadOnly Property PanelCaption() As String
-			Get
-				Return "new document"
-			End Get
-		End Property
-	End Class
+    Partial Public Class DocumentView
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Public ReadOnly Property PanelCaption As String
+            Get
+                Return "new document"
+            End Get
+        End Property
+    End Class
 End Namespace

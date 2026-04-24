fc7063563b4cdf2add30359e49fcec1942c4976f
Andrey Marten <andrey.kunitsyn@devexpress.com>
2026-04-24 13:13:49 +0200

    Update to v26.1 and Prism9

--- VB/PrismOnDXDocking.ExampleModule/Module.vb
+++ VB/PrismOnDXDocking.ExampleModule/Module.vb
@@ -1,75 +1,74 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.ComponentModel.Composition
-Imports System.Linq
-Imports CommonServiceLocator
+﻿Imports System.Linq
 Imports DevExpress.Mvvm
 Imports Prism.Ioc
-'using Microsoft.Practices.ServiceLocation;
-Imports Prism.Mef.Modularity
 Imports Prism.Modularity
-Imports Prism.Regions
+Imports Prism.Navigation.Regions
 Imports PrismOnDXDocking.ExampleModule.Views
 Imports PrismOnDXDocking.Infrastructure
 
 Namespace PrismOnDXDocking.ExampleModule
+    Public Class ExampleModule
+        Implements IModule
 
-	Public Class ExampleModule
-		Implements IModule
-		Private menuService As IMenuService
-		Private regionManager As IRegionManager
+        Private ReadOnly menuService As IMenuService
+        Private regionManager As IRegionManager
+        Private containerProvider As IContainerProvider
 
-		Public Sub New(ByVal menuService As IMenuService)
-			Me.menuService = menuService
-		End Sub
+        Public Sub New(menuService As IMenuService)
+            Me.menuService = menuService
+        End Sub
 
+        Public Sub OnInitialized(containerProvider As IContainerProvider) Implements IModule.OnInitialized
+            Me.containerProvider = containerProvider
+            regionManager = containerProvider.Resolve(Of IRegionManager)()
 
-		Public Sub OnInitialized(ByVal containerProvider As IContainerProvider) Implements IModule.OnInitialized
-			regionManager = containerProvider.Resolve(Of IRegionManager)()
+            regionManager.RegisterViewWithRegion(RegionNames.TabRegion, GetType(DefaultView))
 
-			regionManager.RegisterViewWithRegion(RegionNames.TabRegion, GetType(DefaultView))
+            regionManager.AddToRegion(RegionNames.LeftRegion, containerProvider.Resolve(Of ToolBoxView)())
+            regionManager.AddToRegion(RegionNames.RightRegion, containerProvider.Resolve(Of PropertiesView)())
+            regionManager.AddToRegion(RegionNames.MainRegion, containerProvider.Resolve(Of DocumentView)())
 
-			regionManager.AddToRegion(RegionNames.LeftRegion, ServiceLocator.Current.GetInstance(Of ToolBoxView)())
-			regionManager.AddToRegion(RegionNames.RightRegion, ServiceLocator.Current.GetInstance(Of PropertiesView)())
-			regionManager.AddToRegion(RegionNames.MainRegion, ServiceLocator.Current.GetInstance(Of DocumentView)())
+            menuService.Add(New MenuItem With {.Command = New DelegateCommand(AddressOf ShowOutput), .Parent = "View", .Title = "Output"})
+            menuService.Add(New MenuItem With {.Command = New DelegateCommand(AddressOf ShowProperties), .Parent = "View", .Title = "Properties Window"})
+            menuService.Add(New MenuItem With {.Command = New DelegateCommand(AddressOf ShowToolbox), .Parent = "View", .Title = "Toolbox"})
+            menuService.Add(New MenuItem With {.Command = New DelegateCommand(AddressOf AddNewDocument), .Parent = "File", .Title = "New"})
+        End Sub
 
-			menuService.Add(New MenuItem() With {.Command = New DelegateCommand(AddressOf ShowOutput), .Parent = "View", .Title = "Output"})
-			menuService.Add(New MenuItem() With {.Command = New DelegateCommand(AddressOf ShowProperties), .Parent = "View", .Title = "Properties Window"})
-			menuService.Add(New MenuItem() With {.Command = New DelegateCommand(AddressOf ShowToolbox), .Parent = "View", .Title = "Toolbox"})
-			menuService.Add(New MenuItem() With {.Command = New DelegateCommand(AddressOf AddNewDocument), .Parent = "File", .Title = "New"})
-		End Sub
+        Public Sub RegisterTypes(containerRegistry As IContainerRegistry) Implements IModule.RegisterTypes
 
-		Public Sub RegisterTypes(ByVal containerRegistry As IContainerRegistry) Implements IModule.RegisterTypes
+        End Sub
 
-		End Sub
+        Private Sub AddNewDocument()
+            Show(Of DocumentView)(RegionNames.MainRegion, True)
+        End Sub
 
-		Private Sub AddNewDocument()
-			Show(Of DocumentView)(RegionNames.MainRegion, True)
-		End Sub
-		Private Function GetView(Of T)(ByVal region As IRegion, <System.Runtime.InteropServices.Out()> ByRef view As T) As Boolean
-			view = region.Views.OfType(Of T)().FirstOrDefault()
-			Return view IsNot Nothing
-		End Function
-		'void Initialize() {
-		'    Initialize();
-		'}
-		Private Sub Show(Of T)(ByVal regionName As String, Optional ByVal addNew As Boolean = False)
-			Dim region = regionManager.Regions(regionName)
-			Dim view As T
-			If addNew OrElse (Not GetView(Of T)(region, view)) Then
-				view = ServiceLocator.Current.GetInstance(Of T)()
-				regionManager.AddToRegion(regionName, view)
-			End If
-			region.Activate(view)
-		End Sub
-		Private Sub ShowOutput()
-			Show(Of OutputView)(RegionNames.TabRegion)
-		End Sub
-		Private Sub ShowProperties()
-			Show(Of PropertiesView)(RegionNames.RightRegion)
-		End Sub
-		Private Sub ShowToolbox()
-			Show(Of ToolBoxView)(RegionNames.LeftRegion)
-		End Sub
-	End Class
+        Private Function GetView(Of T)(region As IRegion, ByRef view As T) As Boolean
+            view = region.Views.OfType(Of T)().FirstOrDefault()
+            Return view IsNot Nothing
+        End Function
+
+        Private Sub Show(Of T)(regionName As String, Optional addNew As Boolean = False)
+            Dim region = regionManager.Regions(regionName)
+            Dim view As T
+
+            If addNew OrElse Not GetView(region, view) Then
+                view = containerProvider.Resolve(Of T)()
+                regionManager.AddToRegion(regionName, view)
+            End If
+
+            region.Activate(view)
+        End Sub
+
+        Private Sub ShowOutput()
+            Show(Of OutputView)(RegionNames.TabRegion)
+        End Sub
+
+        Private Sub ShowProperties()
+            Show(Of PropertiesView)(RegionNames.RightRegion)
+        End Sub
+
+        Private Sub ShowToolbox()
+            Show(Of ToolBoxView)(RegionNames.LeftRegion)
+        End Sub
+    End Class
 End Namespace
--- VB/PrismOnDXDocking.ExampleModule/Views/DefaultView.xaml.vb
+++ VB/PrismOnDXDocking.ExampleModule/Views/DefaultView.xaml.vb
@@ -1,21 +1,17 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.ComponentModel.Composition
-Imports System.Windows.Controls
-Imports PrismOnDXDocking.Infrastructure
+﻿Imports System.Windows.Controls
 
 Namespace PrismOnDXDocking.ExampleModule.Views
-	<PartCreationPolicy(CreationPolicy.NonShared), Export> _
-	Partial Public Class DefaultView
-		Inherits UserControl
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class DefaultView
+        Inherits UserControl
 
-		Public ReadOnly Property PanelCaption() As String
-			Get
-				Return "Default View"
-			End Get
-		End Property
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Public ReadOnly Property PanelCaption As String
+            Get
+                Return "Default View"
+            End Get
+        End Property
+    End Class
 End Namespace
